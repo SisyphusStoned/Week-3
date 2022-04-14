91ba21abc9aef3e971635bddd83a55e01f3133a3
Vincent C <vincent.chung@kellogg.northwestern.edu>
2022-04-14 01:35:13 +0000

    crm

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,39 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT
+);
+
+CREATE TABLE activities(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  method TEXT,
+  date TEXT,
+  notes TEXT,
+  contact_id INTEGER,
+  sales_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  industries_id INTEGER
+);
+
+CREATE TABLE sales (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE industries(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  );
+
